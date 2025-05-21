903296316cbad865286b3bff1d21b93fecc1d3f8
lavondedunigan <93886681+lavondedunigan@users.noreply.github.com>
2025-05-20 21:35:40 -0500

    fixed errrrors

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=70)
+    content = models.TextField()
+    image = models.ImageField(upload_to='notes/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""
+Class-based views:
+
+View        = generic view
+ListView    = get a list of records
+DetailView  = get a single(detail) record
+CreateView  = create a new record
+DeleteView  = remove a record
+UpdateView  = modify an existing record
+LoginView   = login
+"""
+
+
+
+# Create your views here.
